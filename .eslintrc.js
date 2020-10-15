d7df5a69c3b80ef3b326029e4daca980197a114e
SevenCats <snowcolton@hotmail.com>
2020-10-15 04:08:53 -0400

    Add initial files

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	root: true,
+	extends: 'airbnb-base',
+	parser: 'babel-eslint',
+	parserOptions: { sourceType: 'script' },
+	env: { node: true },
+	rules: {
+		strict: [2, 'global'],
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+		'arrow-parens': [2, 'as-needed'],
+		'no-console': 0,
+	},
+};
---
+++ knexfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dotenv').config();
+const stringcase = require('knex-stringcase');
+
+module.exports = stringcase({
+	client: 'pg',
+	connection: {
+		host: process.env.POSTGRES_HOST,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DATABASE,
+	},
+});
