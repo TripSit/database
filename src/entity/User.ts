92925608793ce0bdbcd448db05157bed05cb94bc
SevenCatsHere <sevencats@tripsit.me>
2021-07-08 03:36:03 -0400

    add docker compose file

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -5,7 +5,7 @@ import {
   CreateDateColumn,
   BeforeInsert,
 } from 'typeorm';
-import argon2 from 'argon2';
+import { hash, verify } from 'argon2';
 
 type UserRole = 'user'
 | 'alumini'
@@ -68,11 +68,11 @@ export class User {
 
   @BeforeInsert()
   async hashPassword(): Promise<void> {
-    this.password = await argon2.hash(this.password);
+    this.password = await hash(this.password);
   }
 
   async authorize(password: string): Promise<boolean> {
-    return argon2.verify(this.password, password);
+    return verify(this.password, password);
   }
 
   hasPriviledge(role: UserRole): boolean {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
-import { User } from './entity/User';
 
-createConnection()
-  .then(async (connection) => {
-      const user = new User();
-      await connection.manager.save(user);
-      const users = await connection.manager.find(User);
-  })
-  .catch(error => console.log(error));
+export { createConnection } from 'typeorm';
+export { User } from './entity/User';
